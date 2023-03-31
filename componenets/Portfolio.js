4f97c839413c966cfb640b86c5a6234e32d5ace2
MaxSmile <dev@smrt16.com>
2023-03-31 16:26:56 +0300

    ready to implement the animation

--- componenets/Portfolio.js
+++ componenets/Portfolio.js
@@ -1,33 +1,59 @@
+import Image from "next/image";
+
+
+const pojects = [
+    {
+        img: "/img1.jpeg",
+        title: "Garantex",
+        subtitle: "Garantex is a crypto wallet that allows you to manage your crypto portfolio.",
+        descriptio: "The easiest & secure way to send, receive & exchange cryptocurrency. Control & manage your digital assets while you're on the move. Take full control of your tokens everywhere.",
+    },
+    {
+        img: "/img2.jpeg",
+        title: "Assistant",
+        subtitle: "Dashboard, assistant for every day, help you accomplish common tasks quickly.",
+        descriptio: "A tone of bonuses are waiting for you! Earn bonuses with your friends, improve your gaming experience, and get exclusive items!",
+    },
+    {
+        img: "/img3.jpeg",
+        title: "Meditation app",
+        subtitle: "Meditation app that helps you to reach your goals.",
+        descriptio: "You have many options - you can choose your favourite individual meditations, create a playlist, or even take a course consisting of meditations on the same topic. Whichever option you choose - with our meditations you will always be in a good mood, healthy, cheerful, and full of energy.",
+    },
+    {
+        img: "/img4.jpeg",
+        title: "Hydration app",
+        subtitle: "Hydration tracker and heart rate measurement mobile concept.",
+        descriptio: "Connect with your body! You can check your heart rate during sports, physical activity and rest. Schedule a time to take your heart rate to be sure of your health.",
+    }
+
+]
+
 export default function Portfolio() {
     return (
         <div className="bg-white text-sky p-10 lg:p-40">
             <div className="grid gap-y-10 gap-x-8 grid-cols-1 lg:grid-cols-2">
                 <div>
-
-
+                    {
+                        pojects.map((project, index) => (
+                            <div key={index}>
+                                <Image src={project.img} alt="Potfolio" width={380} height={285} />
+                            </div>
+                        ))
+                    }
                 </div>
 
 
                 <div>
-                    Garantex
-
-                    Crypto wallet is the easiest way to manage your cryptocurrency portfolio.
-                    The easiest & secure way to send, receive & exchange cryptocurrency. Control & manage your digital assets while you're on the move. Take full control of your tokens everywhere.
-
-                    Assistant
-
-                    Dashboard, assistant for every day, help you accomplish common tasks quickly.
-                    A tone of bonuses are waiting for you! Earn bonuses with your friends, improve your gaming experience, and get exclusive items!
-
-                    Meditation app
-
-                    The design of this app aims to help users to reach that necessary harmon.
-                    You have many options - you can choose your favourite individual meditations, create a playlist, or even take a course consisting of meditations on the same topic. Whichever option you choose - with our meditations you will always be in a good mood, healthy, cheerful, and full of energy.
-
-                    Hydration app
-
-                    Body hydration tracker and heart rate measurement mobile concept.
-                    Connect with your body! You can check your heart rate during sports, physical activity and rest. Schedule a time to take your heart rate to be sure of your health.
+                    {
+                        pojects.map((project, index) => (
+                            <div key={index} className="py-8 m-10">
+                                <div className="text-gray-500">{project.title}</div>
+                                <div className="text-4xl my-10">{project.subtitle}</div>
+                                <p>{project.descriptio}</p>
+                            </div>
+                        ))
+                    }
                 </div>
 
             </div>
